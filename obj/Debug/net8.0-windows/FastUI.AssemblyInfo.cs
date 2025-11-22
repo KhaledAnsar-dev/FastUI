734da544d57f5da64ab0c3059b4ace1e0ac8e6fa
Khaled Ansar <khaledansar.in@gmail.com>
2025-11-22 07:15:35 +0100

    feat(structure): add initial control files (FastButton, FastTextBox, FastDatePicker, FastComboBox)

--- obj/Debug/net8.0-windows/FastUI.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/FastUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e9601fa32d3b054ed42c8a56d599847ae78266")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2044a9a5398e3e62299fbead64ce34d4b74775b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
